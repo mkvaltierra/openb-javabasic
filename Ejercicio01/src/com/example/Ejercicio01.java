beeaa1bb46e5381b59c338533386656903c3fcba
mkvaltierra <mkvaltierra.web@gmail.com>
2022-02-21 13:07:39 +0100

    first ejercicio01

---
+++ Ejercicio01/src/com/example/Ejercicio01.java
@@ -0,0 +1,30 @@
+package com.example;
+
+public class Ejercicio01 {
+
+    public static void main(String[] args){
+
+        // Variables
+        byte diaSem = 1;
+        short s01 = 7965;
+        int diaAnual = 300;
+        long miliseg = 1298332800;
+        float peso = 4.487415693f;
+        double totalFactura = 10350.678d;
+        char sexo = 'M';
+        boolean casado = false;
+        String texto ="Esto son variables";
+
+
+        // Impresión variables
+        System.out.println(diaSem);
+        System.out.println(s01);
+        System.out.println(diaAnual);
+        System.out.println(miliseg);
+        System.out.println(peso);
+        System.out.println(totalFactura);
+        System.out.println(sexo);
+        System.out.println(casado);
+        System.out.println(texto);
+    }
+}
